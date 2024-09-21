0ba98677e77c1834df101e117ebb8402fba4b401
volodymyrlogika <volodymyr.logika@gmail.com>
2024-09-21 18:00:37 +0300

    lesson 2

--- main.py
+++ main.py
@@ -5,5 +5,19 @@ app = QApplication([])
 
 from window import *
 
+def switch_screen():
+    if answer_btn.text() == 'Відповісти':
+        group_box.hide()
+        result_box.show()
+        answer_btn.setText('Наступне питання')
+    else:
+        result_box.hide()
+        group_box.show()
+        answer_btn.setText('Відповісти')
+
+
+
+answer_btn.clicked.connect(switch_screen)
+
 window.show()
 app.exec_()
--- window.py
+++ window.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QRadioButton, QPushButton, QSpinBox, QLabel, QVBoxLayout, QLineEdit, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QGroupBox, QButtonGroup, QRadioButton, QPushButton, QSpinBox, QLabel, QVBoxLayout, QLineEdit, QHBoxLayout
 
 
 window = QWidget() # створення вікна
@@ -24,15 +24,53 @@ row1.addWidget(time_label)
 question_text = QLabel("mouse")
 
 
-main_line = QVBoxLayout()
-main_line.addLayout(row1)
-main_line.addWidget(question_text, alignment=Qt.AlignCenter)
-
 btn1 = QRadioButton("миша")
 btn2 = QRadioButton("щур")
 btn3 = QRadioButton("мішок")
 btn4 = QRadioButton("мус")
 
+radio_group = QButtonGroup()
+radio_group.addButton(btn1)
+radio_group.addButton(btn2)
+radio_group.addButton(btn3)
+radio_group.addButton(btn4)
+
+group_box = QGroupBox("Варіанти перекладу")
+
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
+row2 = QHBoxLayout()
+
+col1.addWidget(btn1)
+col1.addWidget(btn2)
+
+col2.addWidget(btn3)
+col2.addWidget(btn4)
+
+row2.addLayout(col1)
+row2.addLayout(col2)
+
+group_box.setLayout(row2)
+
+result_box = QGroupBox("Результат") 
+result_text = QLabel("Правильно") # Результат: Правильно/Неправильно
+answer_text = QLabel("миша") # Правильна відповідь
+
+result_col = QVBoxLayout()
+result_col.addWidget(result_text)
+result_col.addWidget(answer_text,  alignment=Qt.AlignCenter, stretch=2)
+
+result_box.setLayout(result_col)
+result_box.hide() # приховує панель з результатом
+
 answer_btn = QPushButton("Відповісти")
+
+# Додаємо всі віджети на головну напрямну лінію
+main_line = QVBoxLayout()
+main_line.addLayout(row1, stretch=1)
+main_line.addWidget(question_text, alignment=Qt.AlignCenter, stretch=2)
+main_line.addWidget(result_box, stretch=5)
+main_line.addWidget(group_box, stretch=5)
+main_line.addWidget(answer_btn, stretch=3)
 
 window.setLayout(main_line)
